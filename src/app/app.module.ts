ea4436e803b9997572825d0604a98ca13d395630
Alejahndro <alejahndro02@gmail.com>
2021-04-26 11:31:13 -0500

    Se agregaron los componentes de lista-empleados y empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { EmpleadoComponent } from './components/lista-empleados/empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadosComponent,
+    EmpleadoComponent,
   ],
   imports: [
     BrowserModule
