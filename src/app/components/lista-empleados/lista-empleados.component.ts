697a0c4ce4ea6884859208c0e1b849489d23898a
Alejahndro <alejahndro02@gmail.com>
2021-04-26 19:23:06 -0500

    Se elaboro el array de empleados

--- src/app/components/lista-empleados/lista-empleados.component.ts
+++ src/app/components/lista-empleados/lista-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-empleados.component.css']
 })
 export class ListaEmpleadosComponent implements OnInit {
-
+  listaEmpleados : Empleado[]=[
+    {folio: 1, nombre : 'Coock', apellido: 'Madison', genero: 'Masculino', salario : 15000},
+    {folio: 2, nombre : 'Stephanie', apellido: 'Anderson', genero: 'Femenino', salario : 50000},
+    {folio: 3, nombre : 'Charlotte', apellido: 'Silva', genero: 'Femenino', salario : 30000},
+    {folio: 4, nombre : 'Tim', apellido: 'Stellman', genero: 'Masculino', salario : 10000},
+    {folio: 5, nombre : 'Cassie', apellido: 'Trovals', genero: 'Femenino', salario : 25000}
+  ]
   constructor() { }
 
   ngOnInit(): void {
