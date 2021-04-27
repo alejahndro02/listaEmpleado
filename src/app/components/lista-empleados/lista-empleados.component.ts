cfc41a2a7422b779880dc8560f9a2148202ec83f
Alejahndro <alejahndro02@gmail.com>
2021-04-26 19:42:40 -0500

    Se crearon los metodos totalEmpleados(), totalFemenino(), totalMasculino()

--- src/app/components/lista-empleados/lista-empleados.component.ts
+++ src/app/components/lista-empleados/lista-empleados.component.ts
@@ -12,11 +12,23 @@ export class ListaEmpleadosComponent implements OnInit {
     {folio: 2, nombre : 'Stephanie', apellido: 'Anderson', genero: 'Femenino', salario : 50000},
     {folio: 3, nombre : 'Charlotte', apellido: 'Silva', genero: 'Femenino', salario : 30000},
     {folio: 4, nombre : 'Tim', apellido: 'Stellman', genero: 'Masculino', salario : 10000},
-    {folio: 5, nombre : 'Cassie', apellido: 'Trovals', genero: 'Femenino', salario : 25000}
-  ]
+    {folio: 5, nombre : 'Cassie', apellido: 'Trovals', genero: 'Femenino', salario : 25000},
+    {folio: 7, nombre : 'Rhichard', apellido: 'Linus', genero: 'Masculino', salario : 45000},
+    {folio: 6, nombre : 'Kushina', apellido: 'Yamamoto', genero: 'Femenino', salario : 10000}
+
+  ] 
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  totalEmpleados(): number{
+    return this.listaEmpleados.length;
+  }
+  totalFemenino(): number{
+    return this.listaEmpleados.filter(list=>list.genero === 'Femenino').length
+  }
+  totalMasculino(): number{
+    return this.listaEmpleados.filter(list=>list.genero === 'Masculino').length
+  }
 }
